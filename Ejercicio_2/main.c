1b51cd5041bd8b8415e88b51c5ea45ec64cfa9bb
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2021-10-01 18:29:05 -0300

    Update main.c

--- Ejercicio_2/main.c
+++ Ejercicio_2/main.c
@@ -143,34 +143,3 @@ int main(void) {
 
     return 0;
 }
-
-// Buenas noches a quién corresponda.
-
-// Al igual que en el ejercicio anterior decidí documentarlo. Esta vez haciendo foco en las correcciones/recomendaciones recibidas en el EJ1.
-// Adjunto un link a donde subí la documentación:
-// https://ejercicio-obligatorio-2.netlify.app/
-// ______________________________________________________________________________
-// Correcciones recibidas EJ1:
-
-// Sobre el .h:
-// -Definiste PI como un literal double, por lo que deberías aprovechar la precisión que te provee, sin importar cómo lo vayas a usar después.
-
-// -Incluí las bibliotecas que vayas a usar en este módulo. Por ejemplo, math.h no tiene nada que hacer en main.h, sí estaría bien incluirla en main.c, en donde efectivamente es utilizada.
-
-// -OJO con las protecciones de archivos con doble guión bajo al inicio y al final. Es una convención de las bibliotecas estándar usar ese formato y podría llegar a pasar que alguna etiqueta ya esté definida. Si sabés lo que estás haciendo, está OK.
-// En general tengo entendido que se usa algo tipo MAIN_H_ o EJ1_MAIN_H_ aclarando el nombre del proyecto.
-
-// Sobre el .c:
-
-// -La documentación de las funciones suele ir en el header, que es el archivo que le llega al usuario para que utilice las funciones, como nos llegan a nosotros las bibliotecas estándar (aunque los fuentes son de código abierto también).
-
-// -Seguí así con los espacios entre operadores, después de las comas, etc. Lo que estaría bueno es que NO haya líneas en blanco innecesarias. De esas tu código está lleno.
-// En particular: nunca van líneas en blanco en la 1er línea de una función, y solo deberían ir para separar bloques de código de distinta funcionalidad.
-
-// - +1 por el uso de macro de función, pero ojo, ¿qué pasa si yo hago `DEG_TO_RAD(89 + 1)'?
-// Revisá cómo sobrepasar ese problema.
-// ______________________________________________________________________________
-
-// De antemano agradezco cualquier comentario, mejoras y recomendaciones de buenas prácticas que me permitan mejorar la calidad del código.
-
-// Nuevamente muchas gracias. Un saludo.
